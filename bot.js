85f0c328688a1a3b96313c1bb4cf19405b5cbafd
ninioon <51967597+ninioon@users.noreply.github.com>
2019-06-19 08:07:03 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.CLient();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+ });
+ 
+ client.on('message', message => {
+    if (message.content === 'ping') {
+      message.reply('pong');
+      }
+});
+
+//Must be this way
+client.login(process.env.BOT_TOKEN)'
